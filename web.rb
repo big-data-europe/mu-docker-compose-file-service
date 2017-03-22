f0e48fb7a9f99df2e598db6d56664839745754dd
Nóra Szepes <nora.szepes@tenforce.com>
2017-03-22 15:28:56 +0100

    Fetching the docker text from the DB and retrieving it in a docker-compose.yml file

--- web.rb
+++ web.rb
@@ -6,9 +6,27 @@ require 'json'
 #### Calls ####
 ###############
 
-get '/:text' do
+get '/:id' do
     headers 'Content-Disposition' => "attachment; filename='docker-compose.yml",
-            'Content-Type' => "application/x-yaml"
+            'Content-Type' => 'application/x-yaml'
+
+    id = params[:id].to_s
+    log.error('Invalid ID:' + id ) if id.empty?
+    error('Invalid ID', status = 400) if id.empty?
+
+    sparql_query = 'select ?content '
+    sparql_query += ' where {'
+    sparql_query += ' ?composeFile a <http://stackbuilder.semte.ch/vocabularies/core/DockerCompose>;'
+    sparql_query += " <http://mu.semte.ch/vocabularies/core/uuid> '#{id}';"
+    sparql_query += ' <http://stackbuilder.semte.ch/vocabularies/core/text> ?content.'
+    sparql_query += ' }'
+
+    log.info("\n" + sparql_query)
+
+    dockerFile = ""
+    query(sparql_query).map do |row|
+        dockerFile += row['content'].to_s
+    end
     status 200
-    "file: " + params[:text].to_s
+    dockerFile.to_s
 end
